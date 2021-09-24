d1b39b1a1f1c90f357762eaf5d1ef1e53d6013a0
Channa Ly <channa.info@gmail.com>
2021-09-24 20:03:51 +0700

    Clean code to explictly add route info along with http VERB when they are not part of REST

--- models/posts_controller.rb
+++ models/posts_controller.rb
@@ -15,10 +15,6 @@ class PostController < ApplicationController
     redirect_to blog_index_path if post_list.exist? 
   end
 
-  def breaking_news
-    @posts = Post.active.breaking.page(params[:page]).per(20)
-  end
-
   def update
     redirect_to posts_path if !current_user.admin?
     @post = Post.active.find(params[:id])
@@ -32,18 +28,24 @@ class PostController < ApplicationController
     end
   end
 
+  # GET /posts/preview
   def preview
     @post = Post.active.find(params[:id])
+  end
+
+  # GET /posts/breaking_news
+  def breaking_news
+    @posts = Post.active.breaking.page(params[:page]).per(20)
+  end
+
+  private
+  def not_found_error
+    render "errors/404.html", status: :not_found
   end
 
   # it should whitelist the params for example: user_id is dangerous and bad client might send the wrong user_id
   def post_params
     filters = params.require(:post).permit!
     filters[:user_id] = current_user.id
-  end
-
-  private
-  def not_found_error
-    render "errors/404.html", status: :not_found
   end
 end
